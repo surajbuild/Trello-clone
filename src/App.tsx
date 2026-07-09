55e8e6d47bb3fa4282d966dee897aa5247e39353
Suraj Kumar <surajkumar44727@gmail.com>
2026-07-09 15:44:40 +0530

    create screens and route them using react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import Auth from "./screens/Auth";
+import Dashboard from "./screens/Dashboard";
+import Board from "./screens/Board";
+
 export default function App() {
   return (
-    <div className="text-4xl">
-      Hi there
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<Auth />} />
+          <Route path="/signin" element={<Dashboard />} />
+          <Route path="/signin" element={<Board />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
